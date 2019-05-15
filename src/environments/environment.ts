281aef706ee16a3ac25bfc9277cba6638d52d1ae
carissacks <carissa.cks@gmailcom>
2019-05-15 09:08:35 +0700

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: false, //kalo gamau ditampilin, jadi true
   firebase: {
     apiKey: "AIzaSyBqTpp58Z0DgIVCdAzru0Tz8RvAvscrfIA",
     authDomain: "uas-pti.firebaseapp.com",
